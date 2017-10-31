0e800f20d2b283d488a763da49f99468ae294bca
Syams225 <ahmadsyams225@gmail.com>
2017-10-31 21:15:30 +0700

    Update vanny.py

--- vanny.py
+++ vanny.py
@@ -1399,32 +1399,7 @@ def bot(op):
                   G.preventJoinByTicket = True
                   kk.updateGroup(G)
                   Ticket = kk.reissueGroupTicket(msg.to)
-
-#------------------------------------------------
-	   elif msg.text == "set":
-                    cl.sendText(msg.to, "Check sider"),
-                    try:
-                        del wait2['readPoint'][msg.to]
-                        del wait2['readMember'][msg.to]
-                    except:
-                        pass
-                    wait2['readPoint'][msg.to] = msg.id
-                    wait2['readMember'][msg.to] = ""
-                    wait2['ROM'][msg.to] = {}
-                    print wait2
-            elif msg.text == "Tercyduk":
-                    if msg.to in wait2['readPoint']:
-                        if wait2["ROM"][msg.to].items() == []:
-                            chiya = ""
-                        else:
-                            chiya = ""
-                            for rom in wait2["ROM"][msg.to].items():
-                                print rom
-                                chiya += rom[1] + "\n"
-
-                        cl.sendText(msg.to, "Nih Dia Siders Nya %s\n\n\nReading point Dibuat date n time:\n[%s]"  % (wait2['readMember'][msg.to],chiya,setTime[msg.to]))
-                    else:
-                        cl.sendText(msg.to, "Tulis Set Dulu Baru Tercyduk Sayang :v")
+		
 #-----------------------------------------------
           elif msg.text in ["All"]:
               if msg.from_ in admin:
