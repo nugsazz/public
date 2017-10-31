6a87397ded30bcf82c5da40bd7da9cc8992430b4
Syams225 <ahmadsyams225@gmail.com>
2017-10-31 22:49:42 +0700

    Youtube.com/c/SYAMSPlayMC

--- Syams.py
+++ Syams.py
@@ -1548,18 +1548,6 @@ def bot(op):
             else:
                 pass
                         
-#-------------------Mid via Tag------------------
-            elif msg.text in ["Mid @"]:
-              if msg.from_ in owner:
-                _name = msg.text.replace("Mid @","")
-                _nametarget = _name.rstrip(' ')
-                gs = cl.getGroup(msg.to)
-                for g in gs.members:
-                    if _nametarget == g.displayName:
-                        random.choice(KAC).sendText(msg.to, g.mid)
-                    else:
-                        pass
-
 #------------------------[Kick]----------------------------
 	
             elif msg.text in ["syams"]:
