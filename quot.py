904a8d9a5f117696e20dc1d22621c9ecd1bf5626
Syams225 <ahmadsyams225@gmail.com>
2017-11-05 13:50:39 +0700

    Update quot.py

--- quot.py
+++ quot.py
@@ -2501,35 +2501,7 @@ def bot(op):
 					cl.sendText(msg.to, "%sseconds" % (elapsed_time))
 					cl.sendText(msg.to, "%sseconds" % (elapsed_time))
    
-#--------------------Cek Sider----------------------
-	      elif msg.text == "vantv":
-                      cl.sendText(msg.to, "Hadir")
-                      try:
-                       del wait2['readPoint'][msg.to]
-                       del wait2['readMember'][msg.to]
-                      except:
-                               pass
-                      now2 = datetime.now()
-                      wait2['readPoint'][msg.to] = msg.id
-                      wait2['readMember'][msg.to] = ""
-                      wait2['setTime'][msg.to] = datetime.strftime(now2,"%H:%M")
-                      wait2['ROM'][msg.to] = {}
-                      print wait2
-
-            elif msg.text == "Ciduk":
-                if msg.to in wait2['readPoint']:
-                   if wait2["ROM"][msg.to].items() == []:
-                      chiya = ""
-                   else:
-                      chiya = ""
-                      for rom in wait2["ROM"][msg.to].items():
-                         print rom
-                         chiya += rom[1] + "\n"
-
-		                 cl.sendText(msg.to, "||===== Di Read Oleh =====||%s\n||=======☞ Vanny ☜=======||\n\n||Pelaku CCTV /👇👇👇||\n%sOrang Ini Agak Gila\n\nKerjaan Nya Cuman Siders\n[%s]"  % (wait2['readMember'][msg.to],chiya,setTime[msg.to]))
-       	        else:
-		                 cl.sendText(msg.to, "vantv Blom Di Ketik Koplak\n\nDASAR PIKUN ♪")
-#------------------------------------------------------------------
+#----------------------------------------------
             elif msg.text in ["Ban"]:
 				if msg.from_ in admin:
 					wait["wblacklist"] = True
